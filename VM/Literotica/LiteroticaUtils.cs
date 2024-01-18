44e8909c9511833fe7ff93958741304c6c1c883c
Kevin Augustino <kevinaugustino@hotmail.com>
2024-01-17 21:45:58 -0500

    Improve literotica URL parsing to support other localizations.

--- VM/Literotica/LiteroticaUtils.cs
+++ VM/Literotica/LiteroticaUtils.cs
@@ -15,10 +15,16 @@ namespace StoryManager.VM.Literotica
 {
     public static class LiteroticaUtils
     {
+        private static readonly ReadOnlyCollection<string> Localizations = new List<string>() {
+            "german", "dutch", "spanish", "french", "italian", "romanian", "portuguese", "other"
+        }.AsReadOnly();
+
         private static readonly ReadOnlyCollection<string> StoryUriFormats = new List<string>()
         {
             // https://www.literotica.com/s/{title}
             @"^(?i)(https?:\/\/)?(www\.)?literotica\.com\/s\/(?-i)(?<Value>[^\?]+).*$",
+            // https://www.{language}.literotica.com/s/{title}
+            $@"^(?i)(https?:\/\/)?(www\.)?({string.Join("|", Localizations.Select(Regex.Escape))})\.literotica\.com\/s\/(?-i)(?<Value>[^\?]+).*$",
             // https://www.literotica.com/stories/showstory.php?url={title}
             @"^(?i)(https?:\/\/)?(www\.)?literotica\.com\/stories\/showstory\.php\?url=(?-i)(?<Value>[^\?&]+).*$",
             // https://i.literotica.com/stories/showstory.php?id={id}
